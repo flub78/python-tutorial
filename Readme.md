195949be5a0244e2542dfef8601ce71ff68dec73
fpeignot <fpeignot@VMX-T450-6390.verimatrix.com>
2017-12-18 15:03:36 +0100

    A few more basic examples

--- Readme.md
+++ Readme.md
@@ -1,7 +1,16 @@
 # Python tutorial
 
-This repo contains a list of small programs created during my study of Python.
+This repository contains a list of small programs created during my study of Python.
 
 There are:
 1. some examples
 2. some tests
+
+# HOW TO
+
+## Run a program
+
+## Run a unit test
+
+* A single test
+* A test suite
---
+++ examples/basic_types.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*
+
+print "Experimentation on python basic types!\n"
+
+age = 42
+age += 2
+
+pi = 3.1415926
+
+a_str = "hello world"
+multi_str = """Hello
+    world.
+    I am leaving.
+    Bye"""
+
+print "str = " + a_str 
+print "multi_str = " + multi_str 
+
+print "age = ", age
+
+print "pi = ", pi
+
+print "bye"
+
