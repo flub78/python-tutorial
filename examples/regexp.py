33ab7387b716d7176981c448a994bfce690a5517
flub78 <frederic.peignot@free.fr>
2024-03-13 09:09:12 +0100

    experiment on regepr

--- examples/regexp.py
+++ examples/regexp.py
@@ -58,3 +58,45 @@ float_reg = r'(\d+)\.(\d+)'
 print ('isFloat ', re.match(float_reg, insideBrackets))
 print ('float ', insideBrackets)
 print ('isDigit', insideBrackets.isdigit())
+
+"""
+comma separated list of integers
+"""
+str1 = "1,2,3,4,5"
+reg = r'(\d+),?'
+
+r = re.match(reg, str1)
+print (r.group(0))
+print (re.findall(reg, str1))
+
+"""
+comma separated list of strings enclosed in single and double quotes
+"""
+str2 = "'a','b','c','d','e'"
+str3 = '"a","b","c","d","e"'
+str6 = '"a", "b", "c",  "d", "e"'
+
+str4=""
+
+str5 = "'a, b"
+
+reg = r'\'(.+?)\',?'
+reg2 = r'\"(.+?)\",?'
+
+# reg to match single quoted or double quoted comma separated strings or empty string
+reg3 = r'\'(.+?)\'|\"(.+?)\"|^$'
+
+print(re.findall(reg, str2))
+print("str2 ",re.match(reg2, str2))
+print(re.findall(reg, str3))
+print("str3 ",re.match(reg2, str3))
+
+print('str2 and reg3', re.findall(reg3, str2))
+print('str3 and reg3', re.findall(reg3, str3))
+print('str4 and reg3', re.findall(reg3, str4))
+
+print('str1 and reg3', re.findall(reg3, str1))
+print('str5 and reg3', re.findall(reg3, str5))
+print('str6 and reg3', re.findall(reg3, str6))
+
+
