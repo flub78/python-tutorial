2223d459ca054fc2ff931f7e95f197b046741217
Flub78 <frederic.peignot@free.fr>
2025-05-18 09:51:53 +0200

    Convertisseur vols de découverte, WIP

--- examples/csv_vd.py
+++ examples/csv_vd.py
@@ -42,7 +42,102 @@ def convert_date(date_str):
     except Exception as e:
         print(f"Erreur lors de la conversion de la date {date_str}: {e}")
         return ''
-    
+
+in_headers = ['n°', 'opérateur', 'Date de vente', 'Type de vol', 'Prix', 'mode règle,', 'Bénéficiaire', 'N° à contacter', 'Nb personnes', 'Date du vol', 'Appareil', 'Pilote', '', '', '', '', '', '']
+
+out_headers = [
+            'id', 'saisie_par', 'date_vente', 'club', 'product', 'beneficiaire',
+            'de_la_part', 'occasion', 'paiement', 'participation', 'beneficiaire_email',
+            'beneficiaire_tel', 'urgence', 'date_planning', 'time_planning',
+            'date_vol', 'time_vol', 'pilote', 'airplane_immat', 'cancelled',
+            'nb_personnes', 'prix'
+        ]
+
+def club(row):
+    prd = product(row)
+
+    if prd == "planeur":
+        return 1
+    if "ulm" in prd:
+        return 2
+    return 3
+
+def beneficiaire(row):
+    res = row[6].title()
+    return res
+
+def paiement(row):
+    res = row[5]
+    return res
+
+def cancelled(row):
+    for element in row:
+        if isinstance(element, str) and 'annul' in element.lower():
+            return 1
+    return 0
+
+'''
+abbeville
+baie
+falaises
+planeur
+abbeville_ulm
+baie_ulm
+falaises_ulm
+''' 
+def product(row):
+    product = row[3]
+    if product.lower().strip()=='planeur' or product.lower().strip()=='vv':
+            return 'planeur' 
+    if "BS" in product.upper():
+        product = "baie"
+    elif "FAL" in product.upper():
+        product = "falaises"
+    elif "ABB" in product.upper():
+        product = "abbeville"
+    elif product.upper() == "ULM":
+        product = "baie"
+    else:
+        product = "unknow product |" + product + "|"
+
+    if 'ULM' in row[3].upper():
+        product += "_ulm"
+          
+    return product
+
+def convert_row(row):
+    print("")
+    nr = []
+
+    nr.append( row[0])
+    nr.append( row[1])
+    nr.append( convert_date(row[2]))
+    nr.append( club(row))
+    nr.append( product(row))
+    nr.append( beneficiaire(row))
+    nr.append( "")      # de la part
+    nr.append( "")      # occasion
+    nr.append( paiement(row))
+    nr.append( "")   # participation
+    nr.append( "")   # email
+    nr.append( "")   # tel
+    nr.append( row[7])   # urgence
+    nr.append( "")   # date planning
+    nr.append( "")   # time planning
+    nr.append( convert_date(row[9])) # date vol
+    nr.append( "")   # time vol
+    nr.append( row[11])   # pilote
+    nr.append( row[10])   # immat
+    nr.append( cancelled(row))  
+    nr.append( row[8])   # nb pers
+    nr.append( row[4])   # prix
+
+    i = 0;
+    for fld in nr:
+        print (out_headers[i], nr[i])
+        i += 1
+    return nr
+
 # declare a dictionary to store the results
 data = {}
 sections = {}
@@ -50,15 +145,7 @@ cvs_data = []
 filename = sys.argv[1]
 result_file = sys.argv[2]
 
-headers = ['n°', 'opérateur', 'Date de vente', 'Type de vol', 'Prix', 'mode règle,', 'Bénéficiaire', 'N° à contacter', 'Nb personnes', 'Date du vol', 'Appareil', 'Pilote', '', '', '', '', '', '']
-fieldnames = [
-            'id', 'saisie_par', 'date_vente', 'club', 'product', 'beneficiaire',
-            'de_la_part', 'occasion', 'paiement', 'participation', 'beneficiaire_email',
-            'beneficiaire_tel', 'urgence', 'date_planning', 'time_planning',
-            'date_vol', 'time_vol', 'pilote', 'airplane_immat', 'cancelled',
-            'nb_personnes', 'prix'
-        ]
-cvs_data.append(fieldnames)
+cvs_data.append(out_headers)
 
 with open(filename, 'r') as csvfile:
     csv_reader = csv.reader(csvfile)
@@ -70,23 +157,10 @@ with open(filename, 'r') as csvfile:
             # continue
             pass
             print("header: ", row)
+        else:
+            cvs_data.append(convert_row(row))
             
         line = line + 1
-
-        fld = 0
-        new_row = []
-        for header in headers:
-            value = row[fld]
-
-            if header == "Date de vente":
-                value = convert_date(value)
-            elif header == "Date du vol":
-                value = convert_date(value)
-
-            new_row.append(value)
-            print(header, ": ", new_row[fld])
-            fld += 1
-        cvs_data.append(new_row)
 
     # Generate output CSV filename by adding _uniq to the basename of the input CSV
     output_csv_path = os.path.splitext(filename)[0] + '_uniq.csv'
